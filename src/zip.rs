04bcc57032c588d489962f26d1895b2d7d6e294b
YdrMaster <ydrml@hotmail.com>
2021-10-20 11:05:41 +0800

    perf: 增加一些 inline 标记

--- src/zip.rs
+++ src/zip.rs
@@ -1,15 +1,18 @@
 ﻿/// 雷达的一个点，压缩到 3 字节
-pub struct PointZipped([u8; 3]);
+pub struct PointZipped(pub [u8; 3]);
 
 impl PointZipped {
+    #[inline]
     pub const fn new(len: u16, dir: u16) -> Self {
         Self([len as u8, dir as u8, ((len >> 8 << 5) | (dir >> 8)) as u8])
     }
 
+    #[inline]
     pub const fn len(&self) -> u16 {
         (self.0[2] as u16 >> 5 << 8) | (self.0[0] as u16)
     }
 
+    #[inline]
     pub const fn dir(&self) -> u16 {
         (((self.0[2] & 0x1f) as u16) << 8) | (self.0[1] as u16)
     }
